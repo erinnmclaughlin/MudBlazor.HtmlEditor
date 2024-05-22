106da1ce626583423180ee5af65cb88bf0a75b8d
Erin McLaughlin <erinmclaughlin219@gmail.com>
2024-05-22 08:52:56 -0400

    use null options by default

--- src/Tizzani.MudBlazor.HtmlEditor/MudHtmlToolbarOptions.razor.cs
+++ src/Tizzani.MudBlazor.HtmlEditor/MudHtmlToolbarOptions.razor.cs
@@ -12,144 +12,129 @@ public partial class MudHtmlToolbarOptions
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option to select different headings and paragraph styles.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool TypographyPicker { get; set; } = true;
+    public bool? TypographyPicker { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option to bold text.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool Bold { get; set; } = true;
+    public bool? Bold { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option to italicize text.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool Italic { get; set; } = true;
+    public bool? Italic { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option to underline text.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool Underline { get; set; } = true;
+    public bool? Underline { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option to strike through text.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool Strike { get; set; } = true;
+    public bool? Strike { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, a color picker for the foreground color will be available in the toolbar.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool ForegroundColorPicker { get; set; } = true;
+    public bool? ForegroundColorPicker { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, a color picker for the background color will be available in the toolbar.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool BackgroundColorPicker { get; set; } = true;
+    public bool? BackgroundColorPicker { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option to insert subscripts and superscripts.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool SubSuperScript { get; set; } = true;
+    public bool? SubSuperScript { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option to align text (e.g., left, center, right, justify).
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool Align { get; set; } = true;
+    public bool? Align { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option to insert an unordered (numbered) list.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool OrderedList { get; set; } = true;
+    public bool? OrderedList { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option to insert an unordered (bulleted) list.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool UnorderedList { get; set; } = true;
+    public bool? UnorderedList { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option of increasing/decreasing the indent size.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool Indent { get; set; } = true;
+    public bool? Indent { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option of inserting a hyperlink.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool InsertLink { get; set; } = true;
+    public bool? InsertLink { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option of inserting an image.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool InsertImage { get; set; } = true;
+    public bool? InsertImage { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option of inserting a blockquote.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool Blockquote { get; set; } = true;
+    public bool? Blockquote { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option of inserting a code block.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool CodeBlock { get; set; } = true;
+    public bool? CodeBlock { get; set; }
 
     /// <summary>
     /// If <see langword="true"/>, the toolbar will include the option of inserting a horizontal rule.
-    /// Default is <see langword="true"/>.
     /// </summary>
     [Parameter]
-    public bool HorizontalRule { get; set; } = true;
+    public bool? HorizontalRule { get; set; }
+
+    private static bool Show(params bool?[] options) => options.Any(o => o is not false);
 
     protected override void OnInitialized()
     {
         if (DefaultOptions != null)
         {
-            TypographyPicker = DefaultOptions.TypographyPicker;
-            Bold = DefaultOptions.Bold;
-            Italic = DefaultOptions.Italic;
-            Underline = DefaultOptions.Underline;
-            Strike = DefaultOptions.Strike;
-            ForegroundColorPicker = DefaultOptions.ForegroundColorPicker;
-            BackgroundColorPicker = DefaultOptions.BackgroundColorPicker;
-            SubSuperScript = DefaultOptions.SubSuperScript;
-            Align = DefaultOptions.Align;
-            OrderedList = DefaultOptions.OrderedList;
-            UnorderedList = DefaultOptions.UnorderedList;
-            Indent = DefaultOptions.Indent;
-            InsertLink = DefaultOptions.InsertLink;
-            InsertImage = DefaultOptions.InsertImage;
-            Blockquote = DefaultOptions.Blockquote;
-            CodeBlock = DefaultOptions.CodeBlock;
-            HorizontalRule = DefaultOptions.HorizontalRule;
+            TypographyPicker ??= DefaultOptions.TypographyPicker;
+            Bold ??= DefaultOptions.Bold;
+            Italic ??= DefaultOptions.Italic;
+            Underline ??= DefaultOptions.Underline;
+            Strike ??= DefaultOptions.Strike;
+            ForegroundColorPicker ??= DefaultOptions.ForegroundColorPicker;
+            BackgroundColorPicker ??= DefaultOptions.BackgroundColorPicker;
+            SubSuperScript ??= DefaultOptions.SubSuperScript;
+            Align ??= DefaultOptions.Align;
+            OrderedList ??= DefaultOptions.OrderedList;
+            UnorderedList ??= DefaultOptions.UnorderedList;
+            Indent ??= DefaultOptions.Indent;
+            InsertLink ??= DefaultOptions.InsertLink;
+            InsertImage ??= DefaultOptions.InsertImage;
+            Blockquote ??= DefaultOptions.Blockquote;
+            CodeBlock ??= DefaultOptions.CodeBlock;
+            HorizontalRule ??= DefaultOptions.HorizontalRule;
         }
     }
 }
